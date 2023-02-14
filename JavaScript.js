ba73339387a6f32c8d78a8498e318b5d3ae0aa43
kabelo-gmj <125395632+kabelo-gmj@users.noreply.github.com>
2023-02-14 10:13:42 -0800

    Files

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+﻿const hamburger = document.querySelector(".hamburger");
+const navMeneu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMeneu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMeneu.classList.remove("active");
+}))
+
+/*Product Slider*/
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.next-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
